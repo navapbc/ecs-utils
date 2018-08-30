7cb2d5b384274c36147699a3455bfe8ef80fa063
Brendan Neutra <brendan@hcgov.us>
2018-08-30 13:55:23 -0700

    minimal reqs

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ import os
 
 from setuptools import find_packages, setup
 
+boto3==1.5.28
+botocore==1.10.73
+pytest==3.4.0
+virtualenv==15.1.0
+INSTALL_DEPS = ['boto3>=1.5.28',
+                'botocore>=1.10.73']
 
 def read(fname):
     """Read a file from the filesystem."""
@@ -17,8 +23,10 @@ setup(
     description="Deployment scripts for ECS",
     license="MIT License",
     keywords="",
+    install_requires=INSTALL_DEPS,
     packages=find_packages(),
     long_description=read("README.md"),
+    python_requires='>=3.5.*'
     entry_points={
         "console_scripts": [
             "kms_create = scripts.kms_create:main",
