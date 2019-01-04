7aa504060c4c9a41c9ec35df469e3e18a85e3c82
N7GW <brendan@hcgov.us>
2019-01-04 15:59:43 -0800

    Bolder colors for light backgrounds

--- scripts/utils.py
+++ scripts/utils.py
@@ -5,11 +5,11 @@ import sys
 
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
+    HEADER = '\033[35m'
+    OKBLUE = '\033[34m'
+    OKGREEN = '\033[32m'
+    WARNING = '\033[33m'
+    FAIL = '\033[31m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
