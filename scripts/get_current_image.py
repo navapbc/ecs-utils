a97f181308498ba83d141e0f039ed17ae2662e78
N7GW <brendan@hcgov.us>
2019-04-03 13:19:46 -0700

    Add a script to get the latest running docker image

---
+++ scripts/get_current_image.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Returns the latest image running in an ECS service
+"""
+import boto3
+import base64
+import argparse
+import sys
+
+from scripts import utils
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='create KMS key')
+
+    parser.add_argument('--region','-r', required=True,
+                        help='AWS region')
+    parser.add_argument('--cluster','-c',
+                        help='ECS cluster name')
+    parser.add_argument('--service','-s',
+                        help='ECS service name')
+    return parser.parse_args()
+
+
+
+def get_ecs_image_url(client, cluster, service):
+    """Gets the current docker image url of an ECS service"""
+
+
+    try:
+        task_definition = client.describe_services(
+            cluster=cluster, services=[service]
+        ).get('services')[0].get('taskDefinition')
+
+        image = client.describe_task_definition(
+            taskDefinition=task_definition
+        ).get('taskDefinition').get('containerDefinitions')[0].get('image')
+    except Exception as err:
+        print(f'Task/Service lookup failed: {err}')
+        sys.exit(1)
+    
+    return image
+
+
+def main():
+    args = parse_args()
+    client = boto3.client('ecs', args.region)
+    print(get_ecs_image_url(client, args.cluster, args.service))
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/get_current_image_test.py
@@ -0,0 +1,44 @@
+"""Test case for get-current-image"""
+import botocore
+import unittest
+from unittest import TestCase
+from unittest.mock import patch
+from scripts.get_current_image import get_ecs_image_url
+
+GOOD_SERVICE = {
+    'services': [{
+        'taskDefinition': 'arn:aws:test_task_definition',
+    }]
+}
+
+GOOD_TASKD = {
+    'taskDefinition': {
+        'containerDefinitions' : [{
+            'image': '123.amazonaws.com/test_service:latest'
+        }]
+    }
+}
+
+
+class ImageTestCase(TestCase):
+    """Test the ecs image utility."""
+
+    @patch('boto3.client')
+    def test_positive(self,mock_boto):
+        mock_client = mock_boto.return_value
+        mock_client.describe_services.return_value = GOOD_SERVICE
+        mock_client.describe_task_definition.return_value = GOOD_TASKD
+        image = get_ecs_image_url(mock_client, 'cluster-foo', 'service-foo')
+        mock_client.describe_task_definition.assert_called_with(taskDefinition='arn:aws:test_task_definition')
+        assert(image == '123.amazonaws.com/test_service:latest')
+
+    @patch('boto3.client')
+    def test_no_task(self,mock_boto):
+        mock_client = mock_boto.return_value
+        mock_client.describe_services.return_value = GOOD_SERVICE
+        mock_client.describe_task_definition.return_value = {}
+        with self.assertRaises(SystemExit):
+            get_ecs_image_url(mock_client, 'cluster-foo', 'service-foo')
+
+if __name__ == '__main__':
+    unittest.main()
